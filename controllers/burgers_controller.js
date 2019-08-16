abb1e989f2a132299992e86cb217a6f9906998ea
geofell <geofell22@gmail.com>
2019-08-16 15:10:42 -0400

    finished initial setup from HW instructions

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var burgers = require("../models/burger.js");
+
+// #### Controller setup
+// 4. Create the `router` for the app, and export the `router` at the end of your file.
---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+  module.exports = app;
